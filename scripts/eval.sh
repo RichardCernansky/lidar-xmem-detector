32fb525b1b70a9046a24ff914221d9e377657b4c
Richard Cernansky <richard.cernansky3@gmail.com>
2026-02-20 15:44:17 +0100

    config edit

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 
 
-python eval.py --cfg_file xmem_det/configs/temporal_pp_xmem_nuscenes.yaml --ckpt log/ckpt/phase1_only_seq8_epoch_8.pth --seq_len 8 --workers 4 --visualize
+python eval.py --cfg_file xmem_det/configs/temporal_pp_xmem_nuscenes.yaml --ckpt log/ckpt/phase0_only_seq8_epoch_27.pth --seq_len 8 --workers 4 --visualize
